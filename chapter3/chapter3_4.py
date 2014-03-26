48866d7c8887d28667d90c0526bf03c1f1ce56a9
kool22 <rnrn3928>
2014-03-26 21:12:21 +0900

    update optimizing

--- chapter3/chapter3_4.py
+++ chapter3/chapter3_4.py
@@ -36,6 +36,35 @@ def score(motif):
     return modif_score
 
 
+def optimize_motif(motifs, k, input_text):
+    result_motif = ''
+    for num in range(0, k):
+        count = {'C': 0, 'G': 0, 'T': 0, 'A': 0}
+        for motif in motifs:
+            count[motif[num]] += 1
+
+        result_ch = ''
+        c = 0
+        for ch in count:
+            if c < count[ch]:
+                result_ch = ch
+                c = count[ch]
+        result_motif += result_ch
+
+    new_motifs = []
+    for line in input_text:
+        _motif = line[0:k]
+        for _k in range(0, len(line) - k + 1):
+            _pattern = line[_k:_k + k]
+            new_count = mismatch(result_motif, _pattern)
+            old_count = mismatch(result_motif, _motif)
+            if new_count < old_count:
+                _motif = _pattern
+        new_motifs.append(_motif)
+
+    return motifs
+
+
 sample_input = '''8 5
 CGCCCCTCTCGGGGGTGTTCAGTAAACGGCCA
 GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG
@@ -57,13 +86,23 @@ best_motifs = rand_motif()
 best_motifs_score = score(best_motifs)
 
 for _k in range(0, random_count):
-    motif = rand_motif()
-    motif_score = score(motif)
+    temp_motif = rand_motif()
+
+    while True:
+        op_motif = optimize_motif(temp_motif, k, input_text)
+        temp_score = score(temp_motif)
+        op_score = score(op_motif)
+        if sum(op_score) < sum(temp_score):
+            temp_motif = op_motif
+        else:
+            break
+
+    motif_score = score(temp_motif)
 
     new_sum = sum(motif_score)
     old_sum = sum(best_motifs_score)
     if new_sum < old_sum:
-        best_motifs = motif
+        best_motifs = temp_motif
         best_motifs_score = motif_score
 
 print best_motifs_score
